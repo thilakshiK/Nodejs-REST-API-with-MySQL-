e195f1a288ad836cdf708c828ca6b275b7f604df
Thilakshi Fonseka <2014com2211@gmail.com>
2019-08-24 17:39:29 +0530

    created database connection

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Nodejs-REST-API-with-MySQL-
+
+This is a simple REST API created with Express js and MySQL
---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express"); //import express module
+const app = express(); // create an express application
+const bodyparser = require("body-parser");
+const mysql = require("mysql");
+
+app.use(bodyparser.json()); //tell the system to use json
+app.use(
+  bodyparser.urlencoded({
+    extended: true
+  })
+);
+
+//create database connection
+
+const dbconn = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "test"
+});
+
+dbconn.connect();
+
+//Get all users
+app.get("/users", (req, res) => {
+  dbconn.query("SELECT * FROM users", (error, results, fields) => {
+    if (error) throw error;
+    return res.send({ error: false, data: results, message: "user list" });
+  });
+});
+
+//set port
+app.listen(5000, () => {
+  console.log("node app is running on port 5000");
+});
