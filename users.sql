e276240d4b9b1c7c4aaf5f7cabcd1aeb5ded6ec7
Thilakshi Fonseka <2014com2211@gmail.com>
2019-08-25 12:45:56 +0530

    SQL file added

--- users.sql
+++ users.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 25, 2019 at 09:10 AM
+-- Generation Time: Aug 25, 2019 at 09:15 AM
 -- Server version: 10.4.6-MariaDB
 -- PHP Version: 7.3.8
 
